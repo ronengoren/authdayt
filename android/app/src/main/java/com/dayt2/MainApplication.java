2b950ce1c761ed46b16f5daa4c7d957f8328bd04
ronengoren <ronen.goren@gmail.com>
2019-07-13 00:41:02 -0400

    merge

--- android/app/src/main/java/com/dayt2/MainApplication.java
+++ android/app/src/main/java/com/dayt2/MainApplication.java
@@ -3,9 +3,7 @@ package com.dayt2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
-
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
@@ -35,12 +33,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
-
-          new RNCViewPagerPackage(),
-
-          new RNLocalizePackage(), new ReactVideoPackage(), new FastImageViewPackage(), new RNFSPackage(),
-          new RNDeviceInfo(), new RNCameraPackage(), new AsyncStoragePackage(), new ReactNativeConfigPackage(),
-          new VectorIconsPackage(), new RNGestureHandlerPackage());
+            new RNCViewPagerPackage(),
+            new RNLocalizePackage(),
+            new ReactVideoPackage(),
+            new FastImageViewPackage(),
+            new RNFSPackage(), new RNDeviceInfo(), new RNCameraPackage(),
+          new AsyncStoragePackage(), new ReactNativeConfigPackage(), new VectorIconsPackage(),
+          new RNGestureHandlerPackage());
     }
 
     @Override
