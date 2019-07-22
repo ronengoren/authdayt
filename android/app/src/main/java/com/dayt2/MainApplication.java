fc97c04ea94d09cb4c58f034a402c5753aa7d877
ronengoren <ronen.goren@gmail.com>
2019-07-21 23:12:25 -0400

    commit

--- android/app/src/main/java/com/dayt2/MainApplication.java
+++ android/app/src/main/java/com/dayt2/MainApplication.java
@@ -3,9 +3,9 @@ package com.dayt2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.github.yamill.orientation.OrientationPackage;
-import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
@@ -33,10 +33,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage(), new OrientationPackage(), new RNCViewPagerPackage(),
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new RNCViewPagerPackage(), new OrientationPackage(),
           new RNLocalizePackage(), new ReactVideoPackage(), new FastImageViewPackage(), new RNFSPackage(),
-          new RNDeviceInfo(), new AsyncStoragePackage(), new ReactNativeConfigPackage(),
-          new VectorIconsPackage(), new RNGestureHandlerPackage());
+          new RNDeviceInfo(), new AsyncStoragePackage(), new ReactNativeConfigPackage(), new VectorIconsPackage(),
+          new RNGestureHandlerPackage());
     }
 
     @Override
